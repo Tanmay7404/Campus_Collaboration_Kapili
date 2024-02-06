de547654a311f7d46fa5974f852bf843ff179879
Tanmay7404 <tanmaymittal7404@gmail.com>
2024-02-06 17:04:42 +0530

    Added Project Functions to add tags, endorsements, tad=gs, feedback, Project

--- backend/controllers/projectFunctions.js
+++ backend/controllers/projectFunctions.js
@@ -1,9 +1,10 @@
 const Project = require("../models/projectModel.js"); // Assuming the correct path to your projectModel file
+const getObjectId = require("../functions/getObjectId.js");
 
 class ProjectController {
     async addProject(project_details) {
         try {
-            const project = new Project({
+            var project = new Project({
                 title: project_details.title,
                 projectInfo: {
                     description: project_details.description,
@@ -21,12 +22,62 @@ class ProjectController {
             });
 
             await project.save();
-            return 1;
+            return project_details._id;
         } catch (err) {
             console.error(err);
             return 0;
         }
     }
+
+    async addCreators(project_id,creators){
+        var project = await Project.findById(project_id);
+        var creatorsId = await getObjectId.userNameToIdList(creators);
+        project.creators = project.creators.concat(creatorsId);
+    }
+
+    async addEndorsements(project_id,endorsements){
+        var project = await Project.findById(project_id);
+        var endorsementsId = await getObjectId.userNameToIdList(endorsements);
+        project.endorsements = project.endorsements.concat(endorsementsId);
+    }
+    async addTags(project_id,tags){
+        var project = await Project.findById(project_id);
+        var tagsId = await getObjectId.tagNameToIdList(tags);
+        project.tags = project.tags.concat(tagsId);
+    }
+
+    async addFeedback(project_id,feedback){
+        var userId = await getObjectId.userNameToId(feedback.reviewer);
+        if(userId == -1){
+            return -1;
+        }
+        else{
+            var newFeedback = {
+                reviewer: userId,
+                message: {
+                    rating: feedback.rating,
+                    text: feedback.test,
+                    timestamp: feedback.timestamp
+                }
+            }
+            var project = Project.findById(project_id);
+            project.feedbacks.push(newFeedback);
+            var n = project.feedbacks.length;
+            project.rating = (project.rating*(n-1) +1)/n;
+            project.save();
+            return 1;
+        }
+    }
+    async changeCompleted(project_id,ongoingStatus){
+        Project.findByIdAndUpdate(project_id,{ongoing: ongoingStatus}, (err)=>{
+            if(err){
+                return -1;
+            }
+            return 1;
+        })
+    }
+    
+        
 }
 
 module.exports = ProjectController;
